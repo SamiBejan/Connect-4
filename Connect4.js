f954d685ca9bcd961252695441437da0d4386715
Samuel Bejan <110773742+SamiBejan@users.noreply.github.com>
2023-05-23 13:55:21 +0300

    Created the table

---
+++ Connect4.js
@@ -0,0 +1,20 @@
+const grid = document.querySelector(".grid");
+let table = new Array(7);
+
+createTable();
+
+//We create the game table
+function createTable() {
+    for (let i = 1; i < table.length; ++i) {
+        table[i] = new Array(8);
+    }
+    for (let i = 1; i < table.length; ++i) {
+        const row = document.createElement("tr");
+        for (let j = 1; j < table[i].length; ++j) {
+            table[i][j] = document.createElement("td");
+            table[i][j].onclick = function() {addPiece(i, j)};
+            row.appendChild(table[i][j]);
+        }
+        grid.prepend(row);
+    }
+}
