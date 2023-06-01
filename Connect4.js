b914eabbcc4f1b6eada482ae2c02a215ffc55864
Samuel Bejan <110773742+SamiBejan@users.noreply.github.com>
2023-06-01 22:31:44 +0300

    Update Connect4.js
    I created the functionality to add yellow and red pieces in the table.

--- Connect4.js
+++ Connect4.js
@@ -1,5 +1,8 @@
 const grid = document.querySelector(".grid");
 let table = new Array(7);
+let piecesCnt = 0, end = false;
+let colLevel = new Array(8);
+colLevel.fill(0);
 
 createTable();
 
@@ -12,9 +15,24 @@ function createTable() {
         const row = document.createElement("tr");
         for (let j = 1; j < table[i].length; ++j) {
             table[i][j] = document.createElement("td");
+            table[i][j].onclick = function() {addPiece(j)};
             row.appendChild(table[i][j]);
         }
         grid.prepend(row);
     }
 }
 
+function addPiece(y) {
+    if (!end && colLevel[y] < 6) {
+        ++piecesCnt;
+        ++colLevel[y];
+        if (piecesCnt % 2 != 0) {
+            table[colLevel[y]][y].setAttribute("id", "red");
+        } else {
+            table[colLevel[y]][y].setAttribute("id", "yellow");
+        }
+        if (piecesCnt === 42) {
+            end = true;
+        }
+    }   
+}
